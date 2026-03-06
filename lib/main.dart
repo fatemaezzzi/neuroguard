124f2947a208d21f05823d89c135e3b3e3143489
krithikanaidu <virtualshakti05@gmail.com>
2026-03-06 14:22:39 +0530

    changes in the all about dementia page

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'core/services/permission_service.dart';
 import 'features/caregiver/cognitest_history_screen.dart';
+import 'features/caregiver/all_about_dementia.dart';
 import 'features/caregiver/caregiver_home.dart';
 import 'core/services/permission_service.dart';
 import 'features/caregiver/vitals_page.dart';
