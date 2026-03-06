387cb01ab6616d147557a67894b13ea3db3f98e5
fatema <fatemaezzi2710@gmail.com>
2026-03-06 14:55:54 +0530

    added navigations on caregiver home

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'core/services/permission_service.dart';
-import 'features/caregiver/cognitest_history_screen.dart';
 import 'features/caregiver/caregiver_home.dart';
-import 'core/services/permission_service.dart';
-import 'features/caregiver/vitals_page.dart';
-import 'features/caregiver/reports_page.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized(); // Required before any async work
-
-  await PermissionService.requestAllPermissions(); // Request mic, camera, location
-
+  WidgetsFlutterBinding.ensureInitialized();
+  await PermissionService.requestAllPermissions();
   runApp(const NeuroGuardApp());
 }
 
@@ -29,7 +23,7 @@ class NeuroGuardApp extends StatelessWidget {
           Theme.of(context).textTheme,
         ),
       ),
-      home: const ReportsPage(),
+      home: const CaregiverHomePage(),
     );
   }
 }
